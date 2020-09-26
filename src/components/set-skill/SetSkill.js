021c63cfc9d219d12d394ef57fc5a1ebe616d921
Rolandas1369 <rolandaswb@gmail.com>
2020-09-26 21:19:41 +0300

    add object to context,

--- src/components/set-skill/SetSkill.js
+++ src/components/set-skill/SetSkill.js
@@ -5,7 +5,7 @@ export default class SetSkill extends Component {
     static contextType = SomeContext;
     render() {
         const value = this.context
-
+        console.log('value', value.items)
         return (
             <div>
                 {value.name}
--- src/context-service/ContextService.js
+++ src/context-service/ContextService.js
@@ -4,9 +4,16 @@ const SomeContext =  React.createContext('defULT')
 const SomeProvider = SomeContext.Provider
 
 class ContextService extends Component {
+
+    state = {
+        items: [
+            {one: '1'},
+            {two: '2'}
+        ]
+    }
     render() {
         return (
-        <SomeProvider value={{name: 'dog'}}>{this.props.children}</SomeProvider>
+        <SomeProvider value={{...this.state}}>{this.props.children}</SomeProvider>
         )
     }
 }
